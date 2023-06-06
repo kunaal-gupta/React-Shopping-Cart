aedc78bf9cfa25bf6164308415f3b5d69bd208b9
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-06-06 12:11:02 -0400

    rendering list

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
 import './Style.css'
 
-const Product = {
+const Product = [
+  {id: 0, name: 'Table', Price: 203, src: ''},
+  {id: 0, name: 'Chair', Price: 40, src: ''},
+  {id: 0, name: 'Television', Price: 60, src: ''},
+  {id: 0, name: 'Mobile', Price: 240, src: '' }
+  ];
 
-  
-}
+
+
+
 
 export function Boxes() {
-  return (
-    <div className='box'> 
-      <p>j</p>
-      <p>Name: {} </p>
-      <p>Price: {} </p>
-    </div>
+  const Inventory = Product.map(Val => 
+    <li className='box' key={Val.id}>
+      <p>Name: {Val.name} </p>
+      <p>Price: { Val.Price} </p>
+    </li>
   );
+
+  return (<ul>{Inventory} </ul>);
+
 }
 
 export function BoxesList() {
   return (
-    <div style={{display: 'flex', marginBottom: '2%'}}>
+    <div style={{ display: 'flex', marginBottom: '2%' }}>
       <Boxes />
-      <Boxes />
-      <Boxes />
-      <Boxes />
-      <Boxes />
+
     </div>
   )
 }
@@ -32,9 +37,9 @@ export function MyButton() {
   return (
     <div>
       <h1 className="Heading"> Welcome to my React app </h1>
-      <BoxesList/>
-      <BoxesList/>
-      <BoxesList/>
+      <BoxesList />
+      <BoxesList />
+      <BoxesList />
     </div>
   );
 }
