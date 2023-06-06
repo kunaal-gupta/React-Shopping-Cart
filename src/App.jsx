a1ac13c5e8d26b975dd9551e6971712e54d825bc
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-06-06 13:24:34 -0400

    adjusted positions of product

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import './Style.css'
 
 const Product = [
-  {id: 0, name: 'Table', Price: 203, src: ''},
-  {id: 0, name: 'Chair', Price: 40, src: ''},
-  {id: 0, name: 'Television', Price: 60, src: ''},
-  {id: 0, name: 'Mobile', Price: 240, src: '' }
-  ];
-
-
+  { id: 0, name: 'Table', Price: 203, src: '' },
+  { id: 1, name: 'Chair', Price: 40, src: '' },
+  { id: 2, name: 'Television', Price: 60, src: '' },
+  { id: 3, name: 'Mobile', Price: 240, src: '' },
+  { id: 4, name: 'Table', Price: 203, src: '' },
+  { id: 5, name: 'Chair', Price: 40, src: '' },
+  { id: 6, name: 'Television', Price: 60, src: '' },
+  { id: 7, name: 'Mobile', Price: 240, src: '' }
+];
 
 
 
 export function Boxes() {
-  const Inventory = Product.map(Val => 
-    <li className='box' key={Val.id}>
+  const Inventory = Product.map(Val =>
+    <li className='box' key={Val.id} style={{display: 'inline-block', marginBottom: '1%'}}>
       <p>Name: {Val.name} </p>
-      <p>Price: { Val.Price} </p>
+      <p>Price: {Val.Price} </p>
     </li>
   );
 
@@ -25,9 +27,8 @@ export function Boxes() {
 
 export function BoxesList() {
   return (
-    <div style={{ display: 'flex', marginBottom: '2%' }}>
+    <div style={{marginBottom: '1%' }}>
       <Boxes />
-
     </div>
   )
 }
@@ -37,8 +38,6 @@ export function MyButton() {
   return (
     <div>
       <h1 className="Heading"> Welcome to my React app </h1>
-      <BoxesList />
-      <BoxesList />
       <BoxesList />
     </div>
   );
