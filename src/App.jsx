c6eb85b9ec57d267df869ccb161d0021ae9ca265
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-06-27 19:49:30 -0600

    adding into array

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import {Heading} from './Gallery.jsx'
-import {Photo} from './Gallery.jsx'
-import {PhotoDetails} from './Gallery.jsx'
+import { useState } from 'react'
 
+let nextid = 1;
 
+let Initartists = [
+{id: 0, name: 'Ram Mohan Bismil'},
+{id: 1, name: 'Subhash Bose'}
+];
 
 function App() {
-  return (
-    <section>
-        <PhotoDetails/>
-        <PhotoDetails/>
-        <PhotoDetails/>
-        <PhotoDetails/>
+  const [name, setName] = useState('');
+  const [artists, setArtist] = useState(Initartists);
 
-    </section>
+  const array = artists.map(person => 
+    <li key={person.id}> {person.name} </li>
+
+  )
+  return (
+    <>
+      <h1>Inspiring People:</h1>
+
+      <input value = {name} onChange = {e => setName(e.target.value)} placeholder='Enter text' type='text'/>
+      <button onClick={() => {setArtist([
+        ...artists,
+        {id: nextid++, name: name}
+        ])
+      }}>Submit </button>
+
+      <ol> {array} </ol>
+    </>
+
 
   )
 }
