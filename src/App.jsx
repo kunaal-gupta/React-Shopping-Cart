f5cf387d60ea105ee9961bfabf11337898ca87d2
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-06-28 16:21:37 -0600

    reduced redundancy in code

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './Style.css'
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { LoadHomepage } from './Homepage';
 
 
 
@@ -21,16 +22,16 @@ const Products = [
 
 ];
 
-export function Boxes() {
+export function CreateProduct() {
   const [currProducts, setProducts] = useState(Products);
 
-  function Add(id) {
+  function AddProduct(id) {
     setProducts(prevProducts => {
       return prevProducts.map(product => {
 
         if (product.id === id) {
- 
-           return {
+
+          return {
             ...product,
             count: product.count + 1
           };
@@ -40,96 +41,56 @@ export function Boxes() {
     })
   }
 
-  function Delete(id) {
+  function DeleteProduct(id) {
     setProducts(prevProduct => {
       return prevProduct.map(product => {
-        if (product.id === id && product.count >0) {
+        if (product.id === id && product.count > 0) {
           return {
             ...product,
-          count: product.count - 1,
-          
-
-        };
-      }
-      else return product;
-    });
-  })
-}
+            count: product.count - 1,
+          };
+        }
+        else return product;
+      });
+    })
+  }
 
   const Inventory = currProducts.map(Product =>
 
-    <li className='box' id = 'box' key={Product.id} >
-      <p id='prodHeading'> 
-        Name: {Product.name} <br/>
+    <li className='productBox' id='productBox' key={Product.id} >
+      <p id='prodHeading'>
+        Name: {Product.name} <br />
         Price: ${Product.Price}  </p>
 
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <button className='Add_Del_button' onClick={() => Add(Product.id)}> + </button>  
-        <FontAwesomeIcon icon={faShoppingCart} style={{marginTop: '3%'}}/>
-        <button className='Add_Del_button' onClick={() => Delete(Product.id)}> - </button>
-      </div><br/>
+        <button className='Add-DelButton' onClick={() => AddProduct(Product.id)}> + </button>
+        <FontAwesomeIcon icon={faShoppingCart} style={{ marginTop: '3%' }} />
+        <button className='Add-DelButton' onClick={() => DeleteProduct(Product.id)}> - </button>
+      </div><br />
 
       {Product.count} in the cart
 
-    </li>
-    
-  );
+    </li>);
 
   return (<ul>{Inventory} </ul>);
 
 }
 
-export function BoxesList() {
-  return (
-    <div style={{ marginBottom: '1%', display: 'flex', justifyContent: 'center' }}>
-      <Boxes />
-    </div>
-  )
-}
 
-
-
-export function Homepage() {
-  function hideHomepage () {
-
-    document.getElementById('loader').style.display = 'block';
-    setTimeout(() =>
-    {
-      document.getElementById('welcomepage').style.display = 'none';
-      document.getElementById('shoppingPage').style.display = 'block'
-
-
-    },2000)
-
-
-
-  }
-
-  return (
-      <div id='welcomepage'>        
-          <h1 style={{fontSize: '320%', marginLeft: '25%', marginTop: '20%', fontFamily: 'Calibri'}}> Welcome to React Shopping Cart </h1> <br />
-          <button className= 'HomepageButton' id='HomepageButton' onClick={hideHomepage}> Start Shopping </button>
-          <div className='loader' id='loader'>
-          </div>
-      </div>
-  );
-}
-
-3
 export function ReactShop() {
   return (
     <>
-      <Homepage />
+      <LoadHomepage />
 
-      <div id = 'shoppingPage' style={{display: 'none'}}>
-        <h1 className="Heading"> Welcome to my React Shopping app </h1> <br />
-        <BoxesList />
-  
+      <div id='ShoppingPage' style={{ display: 'none' }}>
+
+        <h1 className="ShoppingPageHeading"> Welcome to my React Shop </h1> <br />
+
+        <div style={{ marginBottom: '1%', display: 'flex', justifyContent: 'center' }}>
+          <CreateProduct />
+        </div>
+
       </div>
-      {/* <ul> {Products.map((e) => {
-        return <li key={e.id}> {e.name}  {e.count} </li>
-      })}
-      </ul> */}
     </>
   );
 }
---
+++ src/Homepage.jsx
@@ -0,0 +1,25 @@
+
+export function LoadHomepage() {
+    function Loader () {
+  
+      document.getElementById('loader').style.display = 'block';
+      setTimeout(() =>
+      {
+        document.getElementById('WelcomePage').style.display = 'none';
+        document.getElementById('ShoppingPage').style.display = 'block'
+  
+      },2000)
+  
+    }
+  
+    return (
+        <div id='WelcomePage'>        
+            <h1 style={{fontSize: '320%', marginLeft: '25%', marginTop: '20%', fontFamily: 'Calibri'}}> Welcome to React Shopping Cart </h1> <br />
+            <button className= 'HomepageButton' id='HomepageButton' onClick={Loader}> Start Shopping </button>
+            <div className='loader' id='loader'>
+            </div>
+        </div>
+    );
+  }
+  
+  
