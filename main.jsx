9a25ba7502dc98d8d36c1e6d9eaa8b078b2a9c80
Kunaal Gupta <87749508+kunaal-gupta@users.noreply.github.com>
2023-06-29 15:45:29 -0600

    Added main.jsx

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {ReactShop} from './App.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ReactShop />
+  </React.StrictMode>
+)
