a3f06000b2000cd588c231f2497f4c81d99ea86a
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-06-29 15:42:00 -0600

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React-Shopping-Centre",
 
   plugins: [react()],
+  base: "/React-Shopping-Centre",
+
 })
