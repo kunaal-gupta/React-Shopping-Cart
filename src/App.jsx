8dc6f8bad20e630af8a4d06053f65b87db7f7785
Kunaal Gupta <kunaalgupta@hotmail.com>
2023-06-28 19:52:00 -0600

    Added delete functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,32 @@ let nextid = 1;
 
 let Initartists = [
 {id: 0, name: 'Ram Mohan Bismil'},
-{id: 1, name: 'Subhash Bose'}
+{id: 1, name: 'Subhash Bose'},
+{id: 2, name: 'Mohan'},
+{id: 3, name: 'Sohan'},
+{id: 4, name: 'Suhail'}
+
 ];
 
 function App() {
   const [name, setName] = useState('');
   const [artists, setArtist] = useState(Initartists);
 
+  // Deleting objects
   const array = artists.map(person => 
-    <li key={person.id}> {person.name} </li>
+    <li key={person.id}> {person.name} <button onClick={() => {
+      setArtist(artists.filter((a) => a.id !== person.id
+        
+      ))
+    }}>Delete</button> </li>
 
   )
+
   return (
     <>
       <h1>Inspiring People:</h1>
 
+      {/* Adding objects */}
       <input value = {name} onChange = {e => setName(e.target.value)} placeholder='Enter text' type='text'/>
       <button onClick={() => {setArtist([
         ...artists,
